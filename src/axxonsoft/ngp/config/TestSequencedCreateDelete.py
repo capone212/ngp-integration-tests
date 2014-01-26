93c0e12ff69207112bb0a722f5c1d96e8d02cc28
capone212 <anzor.apshev@gmail.com>
2014-01-26 21:43:33 +0400

    implemented multithreaded version

--- src/axxonsoft/ngp/config/TestSequencedCreateDelete.py
+++ src/axxonsoft/ngp/config/TestSequencedCreateDelete.py
@@ -6,15 +6,49 @@ Created on 24.01.2014
 
 import unittest
 import platform
+from multiprocessing import Pool
+import re
 
 from rsg import RSGtool
 from ngpshell import NgpShell
+
+class TestUtilsException(Exception):
+    pass
+
+# class TestUtils(object):
+#     @staticmethod
+#     def assertGreater(first, second, message):
+#         if not (first > second):
+#             raise TestUtilsException(message)
+#     
+#     @staticmethod
+#     def assertEqual(first, second, message):
+#         if not (first == second):
+#             raise TestUtilsException(message)
+#     
+        
+
+def runSingleThread(workerID):    
+    # Create new ipint object
+    rsg = RSGtool(TestEnvirement.getRsgDirectory(), TestEnvirement.getLocalHostName())
+    rsg.create(objectType="Ipint",
+               initialParameters=("Vendor=Axis;Model=P3301;LANAddress=192.168.200.88;DisplayId=%s" % workerID))
+    
+    ipintObjects = rsg.list("Ipint")
+    
+    createdIpDevice = next(deviceRecord for deviceRecord in ipintObjects 
+         if re.match((".+/DeviceIpint.%s" % workerID), deviceRecord['id']))
+    
+    # Delete instantiated device
+    rsg.delete("Ipint", createdIpDevice['id'])
+    
 
 class TestEnvirement(object):
     
     @staticmethod
     def getRsgDirectory():
-        return r"D:\work\ngp_integration_test\tools\rsg\bin\Release"
+        return r"D:\work\ngp_integration_test\tools\rsg\bin\Debug"
+        #return r"C:\Program Files (x86)\AxxonSoft\AxxonSmart\bin"
     
     @staticmethod
     def getNgpShellDirrectory():
@@ -25,8 +59,7 @@ class TestEnvirement(object):
         return  platform.node().upper()
 
 class TestSequencedCreateDelete(unittest.TestCase): 
-    def test_create_then_delete(self):
-        # Save initial objects list
+    def _01_create_then_delete(self):
         ngpShell = NgpShell(TestEnvirement.getNgpShellDirrectory())
         initialObjectsList = ngpShell.repo_config_list()
         
@@ -37,18 +70,32 @@ class TestSequencedCreateDelete(unittest.TestCase):
         
         # Validate new objects created
         self.assertGreater(len(ngpShell.repo_config_list()), len(initialObjectsList), 
-                           "New objects should be created by create command")
+                           "New objects should be created by create command.")
         
         ipintObjects = rsg.list("Ipint")
-        self.assertEqual(len(ipintObjects), 1, "There should be only one IPINT object")
+        self.assertEqual(len(ipintObjects), 1, "There should be only one IPINT object.")
         # Delete instantiated device
         rsg.delete("Ipint", ipintObjects[0]['id'])
         
         # Validate list before and after is equal 
         postObjectsCondition = ngpShell.repo_config_list()
         
-        self.assertListEqual(initialObjectsList, postObjectsCondition,
-                         "Objects lists before and after have to be equal. This objects are seems orphaned: %s" %  (
+        self.assertEqual(initialObjectsList, postObjectsCondition,
+                         "Objects lists before and after have to be equal. This objects are seems orphaned: %s." %  (
+                                set(postObjectsCondition) - set(initialObjectsList)))
+        
+    
+    def test_02_multithreaded_create_and_delete(self):
+        ngpShell = NgpShell(TestEnvirement.getNgpShellDirrectory())
+        initialObjectsList = ngpShell.repo_config_list()
+        
+        p = Pool(processes = 5)
+        p.map(runSingleThread, range(5))
+        
+        # Validate list before and after is equal 
+        postObjectsCondition = ngpShell.repo_config_list()
+        self.assertEqual(initialObjectsList, postObjectsCondition,
+                         "Objects lists before and after have to be equal. This objects are seems orphaned: %s." %  (
                                 set(postObjectsCondition) - set(initialObjectsList)))
         
         
